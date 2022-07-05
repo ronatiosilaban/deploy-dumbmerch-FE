bc5bc9c34e3e7b075596d97ae573228f343f24dc
ronatiosilaban <103517446+ronatiosilaban@users.noreply.github.com>
2022-07-05 14:33:14 +0700

    Update detail-product.js

--- src/components/detail-product.js
+++ src/components/detail-product.js
@@ -109,7 +109,7 @@ function DetailProductComponent() {
             const response = await API.post('/transaction', body, config);
             //   Create variabel for store token payment from response here ...
             const token = response.data.payment.token
-            console.log('data', response);
+            console.log('data', token);
 
 
             // Init Snap for display payment page with token here ...
